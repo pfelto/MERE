0e89d45a02ab84583be6cea6e913557cc72f3a5a
pfelto <paul.r.felton@gmail.com>
2021-12-20 07:00:48 -0500

    completed setup of prettier and writing mark up for MERE2

--- ex02/directdomaccess/src/App.test.js
+++ ex02/directdomaccess/src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders a div with 5 paragraphs in it", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const angleText = screen.getByText("angle:");
+  expect(angleText).toBeInTheDocument();
 });
